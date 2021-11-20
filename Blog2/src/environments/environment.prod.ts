b9552407f5739056a764bac356f1b2dab8c14727
David Weidenbenner <44938475+comic-man@users.noreply.github.com>
2021-11-20 11:53:46 -0600

    Blog2

---
+++ Blog2/src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  firebase: {
+    projectId: 'blog2-95ed6',
+    appId: '1:268013788701:web:a456a9f9cd4182c7dab0f4',
+    storageBucket: 'blog2-95ed6.appspot.com',
+    apiKey: 'AIzaSyBh2t5aW4t51GlGkGNVvPwGg84AQG_9eTc',
+    authDomain: 'blog2-95ed6.firebaseapp.com',
+    messagingSenderId: '268013788701',
+    measurementId: 'G-HR38MF2RGV',
+  },
+  production: true
+};
---
+++ Blog2/src/environments/environment.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: false,
+  firebase {
+  apiKey: "AIzaSyBh2t5aW4t51GlGkGNVvPwGg84AQG_9eTc",
+  authDomain: "blog2-95ed6.firebaseapp.com",
+  projectId: "blog2-95ed6",
+  storageBucket: "blog2-95ed6.appspot.com",
+  messagingSenderId: "268013788701",
+  appId: "1:268013788701:web:a456a9f9cd4182c7dab0f4",
+  measurementId: "G-HR38MF2RGV"
+  };
+};
+
